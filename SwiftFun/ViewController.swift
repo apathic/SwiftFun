db22104ea43f6991324f7a58590865ae855208d0
Brian Estep <bestep@brians-macbook>
2018-02-28 15:46:33 -0500

    Swiftfun

--- SwiftFun/ViewController.swift
+++ SwiftFun/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0;
+    var s = ""
+    
+    @IBOutlet weak var MyNewLabel: UILabel!
+    
+    @IBAction func ResetCounterButton(_ sender: Any) {
+        count = 0
+        view.backgroundColor = UIColor.white
+        MyNewLabel.text = "COUNTER RESET"
+    }
+    
+    @IBAction func ButtonTapped(_ sender: Any) {
+        count = count + 1
+        
+        if count == 10 {
+            MyNewLabel.text = "SUCCESS"
+            view.backgroundColor = UIColor.purple
+            count = 0
+        }
+        else
+        {
+            s = String(count)
+            MyNewLabel.text = "Count: " + s
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //view.backgroundColor = UIColor.red
+        //MyNewLabel.text = "Brian Rocks"
     }
 
     override func didReceiveMemoryWarning() {
