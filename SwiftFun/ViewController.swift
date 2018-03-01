ad0400b752ecc97b36d79e242b1204f784862c22
Brian Estep <bestep@gmail.com>
2018-03-01 13:38:35 -0500

    Can now math

--- SwiftFun/ViewController.swift
+++ SwiftFun/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var count = 0;
+    var count = 0
     var s = ""
+    var d1 = 1.1
+    var d2 = 2.1
     
-    @IBOutlet weak var TopTextField: UITextField!
     @IBOutlet weak var MyNewLabel: UILabel!
+    @IBOutlet weak var TopTextField: UITextField!
     @IBOutlet weak var BottomTextField: UITextField!
+    @IBOutlet weak var addSwitch: UISwitch!
     
     @IBAction func AddButton(_ sender: Any) {
+        
+        if addSwitch.isOn {
+        d1 = Double(TopTextField.text!)! + Double(BottomTextField.text!)!
+        }
+        else {
+            d1 = Double(TopTextField.text!)! - Double(BottomTextField.text!)!
+        }
+        
+        MyNewLabel.text = "\(d1)"
     }
     
     
